a9eda62e5139548ef0908672466cbac85c331118
firkula <136641122+firkula@users.noreply.github.com>
2023-06-20 23:45:11 +0700

    Add support for mobile devices

---
+++ valley_ocean_piano.vb
@@ -0,0 +1,66 @@
+Public Class SpeakUp
+
+Public Shared Sub Main()
+    SayHello()
+End Sub
+
+Public Shared Sub SayHello()
+    Console.WriteLine("Hello!")
+    Console.WriteLine("Let's Speak Up!")
+End Sub
+
+Public Shared Sub AcknowledgeTheImpact()
+    Console.WriteLine("Your voice makes a difference.")
+    Console.WriteLine("You can make a positive impact in the world.")
+    Console.WriteLine("Your opinion is valuable and heard.")
+End Sub
+
+Public Shared Sub SpeakOut()
+    Console.WriteLine("Be brave and don't be afraid to voice your opinion.")
+    Console.WriteLine("Be unapologetically yourself and stand up for what you believe in.")
+    Console.WriteLine("Don't be afraid to challenge the status quo.")
+End Sub
+
+Public Shared Sub RejectInjustice()
+    Console.WriteLine("Don't be afraid to challenge wrong doings.")
+    Console.WriteLine("Be courageous and speak out against injustice.")
+    Console.WriteLine("Don't be afraid to stand up for what is right.")
+End Sub
+
+Public Shared Sub NurtureKindness()
+    Console.WriteLine("Encourage kindness and compassion in the world.")
+    Console.WriteLine("Be a role model for your generation.")
+    Console.WriteLine("Spread love and acceptance.")
+End Sub
+
+Public Shared Sub MakeChange()
+    Console.WriteLine("Actively seek out opportunities to make a positive change.")
+    Console.WriteLine("Take charge of your life and create the world you want to see.")
+    Console.WriteLine("It's never too late to make an impact.")
+End Sub
+
+Public Shared Sub CelebrateSuccess()
+    Console.WriteLine("Make every success count and celebrate your accomplishments.")
+    Console.WriteLine("Be proud of yourself and remember that you can make a difference.")
+    Console.WriteLine("You can change the world for the better.")
+End Sub
+
+Public Shared Sub PracticeGratitude()
+    Console.WriteLine("Take a moment to be grateful for everything you have.")
+    Console.WriteLine("Be thankful for the small things and be appreciative of what you have.")
+    Console.WriteLine("Be humble and remember that you can use your voice to help make the world a better place.")
+End Sub
+
+Public Shared Sub ReachOut()
+    Console.WriteLine("It's okay to not be alone.")
+    Console.WriteLine("Reach out to your community and make friends who can help you fight for what is right.")
+    Console.WriteLine("You are not alone in your fight to make a difference.")
+End Sub
+
+Public Shared Sub EducateYourself()
+    Console.WriteLine("Don’t be afraid to stay informed and learn new things.")
+    Console.WriteLine("Use your voice to educate yourself and others.")
+    Console.WriteLine("Be open to different perspectives and be willing to have difficult conversations.")
+End Sub
+
+End Class
