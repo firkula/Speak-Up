ad4a373cb5ca10b103f407dfa67215d868b2d5e0
firkula <136641122+firkula@users.noreply.github.com>
2023-07-20 18:01:26 +0700

    Refactor CSS styles

---
+++ elephant_apple_mountain.kt
@@ -0,0 +1,26 @@
+// This is a program to 'Speak Up' to inspire and encourage people to use their voice
+
+// Function which prints out inspiring quote to encourage people to speak up
+fun printQuote() {
+    println("The only way to make your voice heard is to use it!")
+}
+
+// Function to motivate people to speak up
+fun motivateSpeakUp() {
+    println("Go ahead, speak up, be heard!")
+    println("No one else can say what's on your mind!")
+}
+
+// Function to remind people that their voice is their power
+fun powerVoiceReminder() {
+    println("Your voice is your power, so don't be afraid to use it!")
+}
+
+// Main function
+fun main() {
+    println("It's time to 'Speak Up'!")
+    printQuote()
+    motivateSpeakUp()
+    powerVoiceReminder()
+    println("Go forth and speak up!")
+}
